6b7704f86b809958758d87ead3e6a1ef4dbba904
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-10-24 10:59:05 +0900

    :memo: docs: ReturnType 수정 + 코드 블럭화

--- src/type.ts
+++ src/type.ts
@@ -1,63 +1,65 @@
-const user_age: number = 20;
+{
+    const user_age: number = 20;
 
-const user_name: string = "zero";
+    const user_name: string = "zero";
 
-const isMale: boolean = true;
+    const isMale: boolean = true;
 
-let isAnyThing: any = "zero";
-isAnyThing = false;
-isAnyThing = "one";
-isAnyThing = undefined;
-isAnyThing = null;
-isAnyThing = "zero";
+    let isAnyThing: any = "zero";
+    isAnyThing = false;
+    isAnyThing = "one";
+    isAnyThing = undefined;
+    isAnyThing = null;
+    isAnyThing = "zero";
 
-const isObject: object = {
-    user_name: "zero",
-    user_age: 20,
-    user_gender: 'male'
-}
+    const isObject: object = {
+        user_name: "zero",
+        user_age: 20,
+        user_gender: 'male'
+    }
 
-const isArray: string[] = [];
-isArray.push("z")
-isArray.push("e")
-isArray.push("r")
-isArray.push("o")
+    const isArray: string[] = [];
+    isArray.push("z")
+    isArray.push("e")
+    isArray.push("r")
+    isArray.push("o")
 
-let isUnknown: unknown = 2;
-isUnknown = {};
+    let isUnknown: unknown = 2;
+    isUnknown = {};
 
-let union: "top" | "left" | "right" | "bottom";
-union = "right";
-union = "left";
-union = "top";
-union = "right";
+    let union: "top" | "left" | "right" | "bottom";
+    union = "right";
+    union = "left";
+    union = "top";
+    union = "right";
 
 
-type Man = true
-type Girl = false
+    type Man = true
+    type Girl = false
 
-type Gender<T> = T extends boolean & true ? Man : Girl
+    type Gender<T> = T extends boolean & true ? Man : Girl
 
-type ZeroGender = Gender<true>
+    type ZeroGender = Gender<true>
 
-const zeroGender: ZeroGender = true;
+    const zeroGender: ZeroGender = true;
 
-type UserInfoType = {
-    userName: string,
-    userAge: number,
-    userGender: boolean,
-    userAddress: string,
-}
+    type UserInfoType = {
+        userName: string,
+        userAge: number,
+        userGender: boolean,
+        userAddress: string,
+    }
 
-type UserInfoName = {
-    user_name: string
-}
+    type UserInfoName = {
+        user_name: string
+    }
 
-type UserInfoAge = {
-    user_age: number
-}
+    type UserInfoAge = {
+        user_age: number
+    }
 
-const userInfo: UserInfoName & UserInfoAge = {
-    user_name: "zero",
-    user_age: 20,
+    const userInfo: UserInfoName & UserInfoAge = {
+        user_name: "zero",
+        user_age: 20,
+    }
 }
--- src/util.ts
+++ src/util.ts
@@ -1,87 +1,93 @@
-enum booleanValue {
-    false,
-    true,
-}
+{
+    enum booleanValue {
+        false,
+        true,
+    }
 
-console.log(booleanValue[0]) // false
-console.log(booleanValue['true']) // 1
+    console.log(booleanValue[0]) // false
+    console.log(booleanValue['true']) // 1
 
-type UserType = {
-    name: string
-    age: number
-    isMale: boolean
-}
+    type UserType = {
+        name: string
+        age: number
+        isMale: boolean
+    }
 
-const userInfo: UserType = {
-    name: "zero",
-    age: 20,
-    isMale: true
-}
+    const userInfo: UserType = {
+        name: "zero",
+        age: 20,
+        isMale: true
+    }
 
-const userPartial: Partial<UserType>[] = [
-    {name: "sugar"},
-    {age: 20},
-    {isMale: false},
-    {}
-]
+    const userPartial: Partial<UserType>[] = [
+        {name: "sugar"},
+        {age: 20},
+        {isMale: false},
+        {}
+    ]
 
-const userOmit: Omit<UserType, "age" | "name"> = {
-    isMale: false,
-}
+    const userOmit: Omit<UserType, "age" | "name"> = {
+        isMale: false,
+    }
 
-const userName: Pick<UserType, "name"> = {
-    name: "sugar"
-}
+    const userName: Pick<UserType, "name"> = {
+        name: "sugar"
+    }
 
-type topWithType = Extract<arrayType, "top" | "left">
+    type topWithType = Extract<arrayType, "top" | "left">
 
-type arrayType = "top" | "left" | "right" | "bottom";
-type topWithOutType = Exclude<arrayType, "top">
+    type arrayType = "top" | "left" | "right" | "bottom";
+    type topWithOutType = Exclude<arrayType, "top">
 
-let isNotType: topWithOutType = "bottom";
-isNotType = "right"
+    let isNotType: topWithOutType = "bottom";
+    isNotType = "right"
 
-type MakeUserType = ReturnType<(userInfo: UserType) => void>
+    type MakeUserType = ReturnType<(userInfo: UserType) => void>
 
-function UserMarker(newUser: UserType): MakeUserType {
-    console.log(newUser)
-    return;
-}
+    function UserMarker(newUser: UserType): MakeUserType {
+        console.log(newUser)
+        return;
+    }
 
-UserMarker({age: 20, isMale: true, name: "zero"});
+    UserMarker({age: 20, isMale: true, name: "zero"});
 
-type StudentType = {
-    number?: number,
-    name: string,
-}
+    type UserInfoName = ReturnType<(student: StudentType) => string>
 
-const st_zero: StudentType = {
-    number: 0, name: "zero"
-}
+    const std_name:UserInfoName = 'zero'
 
-const st_one: StudentType = {
-    number: 1, name: "sugar"
-}
+    type StudentType = {
+        number?: number,
+        name: string,
+    }
 
-const teacher: StudentType = {
-    name: "강선생"
-}
+    const st_zero: StudentType = {
+        number: 0, name: "zero"
+    }
 
-const Human = {
-    name: "zero",
-    age: "30",
-    wishMovieList: ["aboutTime", "noteBook"]
-} satisfies Record<'name' | 'age' | 'wishMovieList', string | string[]>
+    const st_one: StudentType = {
+        number: 1, name: "sugar"
+    }
+
+    const teacher: StudentType = {
+        name: "강선생"
+    }
+
+    const Human = {
+        name: "zero",
+        age: "30",
+        wishMovieList: ["aboutTime", "noteBook"]
+    } satisfies Record<'name' | 'age' | 'wishMovieList', string | string[]>
 
 
-type UserGenericType<T, U, C> = {
-    name: T
-    age: U,
-    isMan: C
-}
+    type UserGenericType<T, U, C> = {
+        name: T
+        age: U,
+        isMan: C
+    }
 
-const newUser1: UserGenericType<string, number, boolean> = {
-    name: "zero",
-    age: 20,
-    isMan: true
+    const newUser1: UserGenericType<string, number, boolean> = {
+        name: "zero",
+        age: 20,
+        isMan: true
+    }
 }
